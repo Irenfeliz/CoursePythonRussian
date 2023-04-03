fc8ecbc1f9a37dfb70595d4c6741df9951d908c0
Irenfeliz <irenfeliz@gmail.com>
2023-04-04 00:54:19 +0300

    commit

--- Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (10)/task.py
+++ Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (10)/task.py
@@ -1,1 +1,12 @@
 word = input() + ' запретил букву'
+# chr(1072): chr(1103)
+alphabet = [chr(i) for i in range(1072, 1104)]
+# print(alphabet)
+
+for i in alphabet:
+    if i in word:
+        print(word.strip() + ' ' + i)
+        word = word.replace(i, '')
+        if '  ' in word:
+            word = word.replace('  ', ' ')
+
